1ca485ddb106e787ac1295e354a7938e9368d295
centpays-tech <163097188+centpays-tech@users.noreply.github.com>
2024-05-20 13:01:06 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/transactionflow")
+mongoose.connect("mongodb+srv://Centpays:Centpays2024@centpays.bao7mvs.mongodb.net/?retryWrites=true&w=majority&appName=Centpays")
